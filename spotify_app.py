afceb92b429914a7f4aa79955e281b9d2517059d
Brandon <torio.brandon@gmail.com>
2024-12-18 17:08:58 -0600

    Update spotify_app.py

--- spotify_app.py
+++ spotify_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from spotipy.oauth2 import SpotifyClientCredentials
+import os
+os.system("pip install --upgrade pip setuptools wheel")
 
 # Fetch credentials from Streamlit secrets
 CLIENT_ID = st.secrets["CLIENT_ID"]
